c575030d9ad2509ad992f1774da4ea2854bb535d
Harshit Yadav <87258190+TGH75972@users.noreply.github.com>
2026-06-04 22:35:08 +0530

    Update ModDataComponentTypes.java

--- src/main/java/net/pocket/mod/component/ModDataComponentTypes.java
+++ src/main/java/net/pocket/mod/component/ModDataComponentTypes.java
@@ -5,9 +5,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 public class ModDataComponentTypes{
-public static final ComponentType<BlockPos> BOOKMARKED_POS = Registry.register(Registries.DATA_COMPONENT_TYPE,Identifier.of("voidcantation", "bookmarked_pos"),ComponentType.<BlockPos>builder().codec(BlockPos.CODEC).packetCodec(net.minecraft.network.codec.PacketCodecs.codec(BlockPos.CODEC)).build());
-public static final ComponentType<VoidStorageData> VOID_STORAGE = Registry.register(Registries.DATA_COMPONENT_TYPE,Identifier.of("voidcantation", "void_storage"),ComponentType.<VoidStorageData>builder().codec(VoidStorageData.CODEC).packetCodec(net.minecraft.network.codec.PacketCodecs.codec(VoidStorageData.CODEC)).build());
+public static final ComponentType<BlockPos> BOOKMARKED_POS = Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of("voidcantation", "bookmarked_pos"), ComponentType.<BlockPos>builder().codec(BlockPos.CODEC).packetCodec(net.minecraft.network.codec.PacketCodecs.codec(BlockPos.CODEC)).build());
+public static final ComponentType<VoidStorageData> VOID_STORAGE = Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of("voidcantation", "void_storage"), ComponentType.<VoidStorageData>builder().codec(VoidStorageData.CODEC).packetCodec(net.minecraft.network.codec.PacketCodecs.codec(VoidStorageData.CODEC)).build());
+public static final ComponentType<PhaseDaggerData> PHASE_DAGGER_DATA = Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of("voidcantation", "phase_dagger_data"), ComponentType.<PhaseDaggerData>builder().codec(PhaseDaggerData.CODEC).packetCodec(net.minecraft.network.codec.PacketCodecs.codec(PhaseDaggerData.CODEC)).build());
 public static void registerComponents(){
-
  }
 }
